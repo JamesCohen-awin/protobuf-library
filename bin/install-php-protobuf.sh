598d3e30a6bafc5e1d7402c317d2351c27cd2516
James Cohen <james.cohen@affiliatewindow.com>
2015-04-23 16:07:47 +0100

    add the .so to the config as described in http://docs.travis-ci.com/user/languages/php/

--- bin/install-php-protobuf.sh
+++ bin/install-php-protobuf.sh
@@ -1,9 +1,8 @@
 # installing the php-protobuf module
-cd /tmp
 curl https://codeload.github.com/allegro/php-protobuf/tar.gz/$PHP_PROTOBUF_VERSION | tar -xvz
 
 cd php-protobuf-$PHP_PROTOBUF_VERSION
 phpize && ./configure && make install
-echo extension=protobuf.so > /etc/php5/cli/conf.d/protobuf.ini
-cd ..
-rm -rf php-protobuf-$PHP_PROTOBUF_VERSION
+
+echo extension=protobuf.so > /tmp/protobuf.ini
+phpenv config-add /tmp/protobuf.ini
