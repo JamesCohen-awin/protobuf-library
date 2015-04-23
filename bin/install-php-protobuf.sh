887c90530d83f9c0fc677858e5a19a47be519abf
James Cohen <james.cohen@affiliatewindow.com>
2015-04-23 16:14:17 +0100

    create a version independent symlink for the php-probuf library

--- bin/install-php-protobuf.sh
+++ bin/install-php-protobuf.sh
@@ -1,8 +1,11 @@
 # installing the php-protobuf module
 curl https://codeload.github.com/allegro/php-protobuf/tar.gz/$PHP_PROTOBUF_VERSION | tar -xvz
 
-cd php-protobuf-$PHP_PROTOBUF_VERSION
+ln -s php-protobuf-$PHP_PROTOBUF_VERSION php-protobuf
+cd php-protobuf
+
 phpize && ./configure && make install
 
 echo extension=protobuf.so > /tmp/protobuf.ini
 phpenv config-add /tmp/protobuf.ini
+
